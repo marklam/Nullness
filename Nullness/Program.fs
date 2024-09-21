919bdac33841e32fcc82dbb1d9bbb0c0765cad48
Mark Lambert <marklam@spunow.co.uk>
2024-09-21 12:25:46 +0100

    Demo of changes for https://github.com/dotnet/fsharp/issues/17776

---
+++ Nullness/Program.fs
@@ -0,0 +1,41 @@
+﻿module Program
+
+open FSharp.UMX
+open FsToolkit.ErrorHandling
+
+type [<Measure>] m
+type [<Measure>] r
+
+type P = | P
+type S = | S
+
+let doThing (a: float<m>) (b:float<m>) =
+    ()
+
+let x (mr : (float<m>*float<r>) option) : unit =
+    option {
+        // Needs annotation
+        //let! (m:float<m>,r:float<r>) = mr
+        let! (m, r) = mr
+
+        let r = UMX.cast<r, m>r
+        doThing r m
+    }
+    |> ignore
+
+let y (s : S option ) (p : P option)=
+    option {
+        // Needs annotation
+        //let! (_s : S) = s
+        let! _s = s
+
+        // Needs annotation
+        //let! (_p : P) = p
+        let! _p = p
+
+        ()
+    } |> ignore
+
+[<EntryPoint>]
+let main argv =
+    0
